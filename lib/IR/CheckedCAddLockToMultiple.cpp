c6ac611e0a3bba08c0480f6619c3d9e1c6ebdc9a
Jie Zhou <jzhou41@cs.rochester.edu>
2021-05-04 13:51:16 -0400

    [_multiple] Moved the lock creation for _multiple stack vars to the pass
    Moved the code that generates a lock for _multiple stack variables from
    the clang side (https://github.com/jzhou76/checkedc-clang/commit/e5eab3120d513be375f7a7e8553c371be0c8b957) to the pass that
    does the lock generation for both stack and global variables.
    
    No functionality changed.

--- lib/IR/CheckedCAddLockToMultiple.cpp
+++ lib/IR/CheckedCAddLockToMultiple.cpp
@@ -13,6 +13,8 @@
 
 #include "llvm/IR/CheckedCAddLockToMultiple.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include <vector>
 
 using namespace llvm;
@@ -23,9 +25,50 @@ CheckedCAddLockToMultiplePass::CheckedCAddLockToMultiplePass() : ModulePass(ID) 
 }
 
 //
-// Entrance of this pass.
+// This function replaces each _multiple stack variable with a struct that
+// contains a lock and the orignal stack variable. The lock for all stack
+// variables is 1. It also replaces all the uses of the original Alloca with
+// a GEP to the stack variable in the struct.
 //
-bool CheckedCAddLockToMultiplePass::runOnModule(Module &M) {
+static bool AllocateLockForMultipleStackVars(Module &M) {
+  std::vector<AllocaInst *> MultipleStackVars;
+  for (Function &Fn : M) {
+    if (Fn.isDeclaration()) continue;
+    // Collect all _multiple stack variables. We only need to iterate over
+    // the front BB because all AllocaInst are in it.
+    for (Instruction &I : Fn.front()) {
+      if (AllocaInst *Alloca = dyn_cast<AllocaInst>(&I)) {
+        if (Alloca->isMultipleQualified()) {
+          MultipleStackVars.push_back(Alloca);
+        }
+      }
+    }
+  }
+
+  // Process each _multiple AllocaInst.
+  IRBuilder<> Builder(M.getContext());
+  for (AllocaInst *Alloca : MultipleStackVars) {
+    Builder.SetInsertPoint(Alloca);
+    StructType *ST = StructType::get(Builder.getInt64Ty(),
+                                     Alloca->getAllocatedType());
+    AllocaInst *NewAlloca = Builder.CreateAlloca(ST);
+    Value *VarPtr = Builder.CreateStructGEP(NewAlloca, 1);
+    // Initialize the lock to 1.
+    Builder.CreateStore(Builder.getInt64(1), Builder.CreateStructGEP(NewAlloca, 0));
+    // Replace the uses of Alloca with the GEP to the new Alloca.
+    BasicBlock::iterator BI(Alloca);
+    ReplaceInstWithValue(Alloca->getParent()->getInstList(), BI, VarPtr);
+  }
+
+  return MultipleStackVars.empty();
+}
+
+//
+// This function replaces each _multiple global (including static local)
+// variabels with a struct that contains a lock and the original global var.
+// The lock for all global variables is 2.
+//
+static bool AllocateLockForMultipleGlobals(Module &M) {
   // Collect _multiple global variables, including static local variables.
   std::vector<GlobalVariable *> MultipleGV;
   for (GlobalVariable &GV : M.globals()) {
@@ -68,6 +111,15 @@ bool CheckedCAddLockToMultiplePass::runOnModule(Module &M) {
   }
 
   return MultipleGV.empty();
+}
+
+//
+// Entrance of this pass.
+//
+bool CheckedCAddLockToMultiplePass::runOnModule(Module &M) {
+  bool hasMultipleStackVars = AllocateLockForMultipleStackVars(M);
+  bool hasMultipleGlobals = AllocateLockForMultipleGlobals(M);
+  return hasMultipleStackVars || hasMultipleGlobals;
 }
 
 char CheckedCAddLockToMultiplePass::ID = 0;
